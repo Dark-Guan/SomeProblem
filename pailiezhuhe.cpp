910d0dc79f681dbdeabe44ba19cb245afaf40616
Dark <dark_guan@foxmail.com>
2020-05-04 23:01:59 +0800

    upload pailie zhu he

---
+++ pailiezhuhe.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char ** args){
+    int m, n, k;
+    long sum = 0;
+    cin >> m >> n >> k;
+
+    int n = n - m*k;
+    if(n <= 0){
+        sum = 0;
+        
+    }else{
+        for()
+    }
+
+
+    sum %= 5201314;
+    cout << sum << endl;
+    return 0;
+}
+
