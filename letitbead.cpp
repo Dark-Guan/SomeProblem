2cb6a1f659739dfec7011c65ff1da3afe0bc2424
Dark <dark_guan@foxmail.com>
2020-05-06 00:38:15 +0800

    add let it bead

--- letitbead.cpp
+++ letitbead.cpp
@@ -3,28 +3,46 @@
 #include <vector>
 using namespace std;
 
-int FindDiffClass(int start, string res, vector < string > resList, int &end, int &numColor)
+int FindDiffClass(int start, string res, vector<string> &resList, int &end, int &numColor)
 {
 	if (start == end)
 	{
 		bool findSame = false;
-		for(int j = 0; j < end; j ++){
-		for(int i = 0; i < resL.size(); i ++){
-			
-			for(int j = 0; j < end; j ++){
-				
-			}
-		}
-		if ()
+		int j = end;
+		string temp = res;
+		int len = temp.size();
+		do
 		{
+			for (int i = 0; i < resList.size(); i++)
+			{
+				if(temp == resList[i]){
+					findSame = true;
+					break;
+				}
+			}
+			if(findSame){
+				break;
+			}
+			if(temp.size() > 1){
+				temp = temp + temp;
+				temp = temp.substr(1, len);
+			}
+			j--;
+		}while (j);
 
+		if (findSame)
+		{
+			return 0;
+		}else{
+			resList.push_back(res);
+			return 1;
 		}
 	}
 	int num = 0;
 	for (int i = 0; i < numColor; i++)
 	{
 		res.push_back('1' + i);
-		num += FindDiffClass(start + 1,);
+		num += FindDiffClass(start + 1, res, resList, end, numColor);
 		res.pop_back();
 	}
 	return num;
@@ -34,9 +52,10 @@ int main()
 {
 	int n, m;
 	string res;
-	vector < string > resL;
+	vector<string> resL;
 	cin >> n >> m;
 
-	vector int a = FindDiffClass(0, res, resL, m, n);
+	int a = FindDiffClass(0, res, resL, m, n);
+	cout << a << endl;
 	return 0;
 }
