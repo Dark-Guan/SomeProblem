e2e7a3441a0c8e86368aac77223feea5334a3589
Dark <dark_guan@foxmail.com>
2020-05-10 15:00:08 +0800

    add new solve of RunPartern

--- RunPattern.cpp
+++ RunPattern.cpp
@@ -4,32 +4,47 @@
 
 using namespace std;
 
+struct Point{
+    int x;
+    int y;
+    bool isMatch;
+};
 
-long FindTheWay(int x, int y, vector<string> &pattern, string route, int endx, int endy){
-    if(x == endx && y == endy){
-        int acState = 0;
+long FindTheWay(Point startP, vector<string> &pattern, string route, Point endP){
+    int acState = 0;
+    if(!startP.isMatch){
         for(int i = 0; i < pattern.size(); i ++){
             int ret = route.find(pattern[i], 0);
             if(ret >= 0){
                 acState ++;
             }
         }
-        if(acState >= 2){
+        if(acState >= pattern.size()){
+            startP.isMatch = true;
+        }
+    }
+    
+    if(startP.x == endP.x && startP.y == endP.y){
+        if(startP.isMatch){
             return 1;
         }else{
             return 0;
         }
     }
-    if(x < 0 || y < 0 || x > endx || y > endy){
+    if(startP.x < 0 || startP.y < 0 || startP.x > endP.x || startP.y > endP.y){
         return 0;
     }
 
     route.push_back('R');
-    long R = FindTheWay(x + 1, y, pattern, route, endx, endy);
+    startP.x += 1;
+    long R = FindTheWay(startP, pattern, route, endP);
+    startP.x -= 1;
     route.pop_back();
     
     route.push_back('D');
-    long D = FindTheWay(x, y + 1, pattern, route, endx, endy);
+    startP.y += 1;
+    long D = FindTheWay(startP, pattern, route, endP);
+    startP.y -= 1;
     route.pop_back();
     return  (R + D);
 }
@@ -46,7 +61,18 @@ int main(int argc, char ** args){
         pattern.push_back(temp);
         cin >> temp;
         pattern.push_back(temp);
-        long sum = FindTheWay(0, 0, pattern, route, n, m);
+        Point startP;
+        Point endP;
+        startP.x = 0;
+        startP.y = 0;
+        startP.isMatch = false;
+
+        endP.x = n;
+        endP.y = m;
+        endP.isMatch = false;
+
+        long sum = FindTheWay(startP, pattern, route, endP);
+        cout << sum << endl;
         sum %= 1000000007;
         cout << sum << endl;
     }
