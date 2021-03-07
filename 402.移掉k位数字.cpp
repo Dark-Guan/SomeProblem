ef511fd49dd4bb1ff7de405253efa95efbf9c1d8
Dark <dark_guan@foxmail.com>
2021-03-07 11:04:54 +0800

    提交需改

---
+++ 402.移掉k位数字.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=402 lang=cpp
+ *
+ * [402] 移掉K位数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+
+    }
+};
+// @lc code=end
+
+int main(){
+
+    return 0;
+}
+
