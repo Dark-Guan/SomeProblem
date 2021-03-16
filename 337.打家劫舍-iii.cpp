ae23a15130f0bc12624b747c8244bd4f630b0703
Dark <dark_guan@foxmail.com>
2021-03-17 02:32:40 +0800

    完成一题二叉树题目

--- 337.打家劫舍-iii.cpp
+++ 337.打家劫舍-iii.cpp
@@ -3,6 +3,16 @@
  *
  * [337] 打家劫舍 III
  */
+#include <unordered_map>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
 // @lc code=start
 /**
@@ -14,11 +24,67 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
-public:
-    int rob(TreeNode* root) {
 
+struct robState
+{
+    int beRobbed;
+    int notBeRobbed;
+};
+
+class Solution
+{
+    robState dfs(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return {0, 0};
+        }
+        robState l = dfs(root->left);
+        robState r = dfs(root->right);
+        int beRobbed = root->val + l.notBeRobbed + r.notBeRobbed;
+        int notBeRobbed = max(l.beRobbed, l.notBeRobbed) + max(r.beRobbed, r.notBeRobbed);
+
+        return {beRobbed, notBeRobbed};
+    }
+
+public:
+    int rob(TreeNode *root)
+    {
+        robState state = dfs(root);
+
+        return max(state.beRobbed, state.notBeRobbed);
     }
 };
 // @lc code=end
 
+int main(int argc, char **args)
+{
+    return 0;
+}
+
+/*
+class Solution
+{
+    unordered_map<TreeNode *, int> beRobbed, notBeRobbed;
+
+    void dfs(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        dfs(root->left);
+        dfs(root->right);
+        beRobbed[root] = root->val + notBeRobbed[root->left] + notBeRobbed[root->right];
+        notBeRobbed[root] = max(beRobbed[root->left], notBeRobbed[root->left]) + max(beRobbed[root->right], notBeRobbed[root->right]);
+    }
+
+public:
+    int rob(TreeNode *root)
+    {
+        dfs(root);
+
+        return max(beRobbed[root], notBeRobbed[root]);
+    }
+};
+*/
